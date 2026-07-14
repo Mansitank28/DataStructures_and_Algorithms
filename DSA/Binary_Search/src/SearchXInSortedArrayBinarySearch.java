be74203ee4a1cfde20156904bcd61586275441eb
manishatank28 <manitank28@gmail.com>
2026-07-14 19:04:05 +0530

    Binary Search-Search X in sorted array

---
+++ DSA/Binary_Search/src/SearchXInSortedArrayBinarySearch.java
@@ -0,0 +1,29 @@
+
+public class SearchXInSortedArrayBinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1,0,3,5,9,0};
+        int target = 9;
+
+        int result = search(nums,target);
+        System.out.println("i======="+result);
+    }
+
+    private static int search(int[] nums, int target) {
+
+        int low=0,high = nums.length-1;
+
+        while(low <= high){
+
+            int mid = (low+high) /2;
+            if(nums[mid] == target)
+                return mid;
+            else if(target > nums[mid])
+                low = mid+1;
+            else
+                high = mid -1;
+        }
+        return -1;
+    }
+}
